1979f4ab3fc36905daca0ec96616433ef3f56ac6
felisucoibi <info@justdust.es>
2019-06-03 20:44:34 +0200

    Update ytbuild.sh

--- ytbuild.sh
+++ ytbuild.sh
@@ -2,7 +2,7 @@
 # JUST A SMALL SCRIPT TO ONE CLICK UBUNTU BUILDING OF YOUTUBE-DL BY felisucoibi
 
 if [ ! -d "./youtube-dl" ]; then
-  sudo apt-get install git pandoc
+  sudo apt-get install git pandoc zip
   mkdir ./youtube-dl
   cd ./youtube-dl
   git clone https://github.com/felisucoibi/youtube-dl.git master
